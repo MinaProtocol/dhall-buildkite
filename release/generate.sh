17395c190155c9b698292086520465238e1c50f2
dkijania <dariusz@o1labs.org>
2025-03-29 22:31:59 +0100

    add release scripts

--- release/generate.sh
+++ release/generate.sh
@@ -14,30 +14,24 @@ latest_tag=$(git describe --tags $(git rev-list --tags --max-count=1))
 
 echo "Latest GitHub tag: $latest_tag"
 
-rm -rf releases/$latest_tag
+# Copy files
 
+rm -rf releases/$latest_tag
 mkdir -p releases/$latest_tag/docs
 
 cp release/template/index.html releases/index.html
 cp release/template/changelog.md releases/$latest_tag/changelog.md
-cp -R src releases/$latest_tag/src
 
+cp -R out/src releases/$latest_tag/src
 
 
 output_file="releases/$latest_tag/package.dhall"
 
+# Generate documentation  
+
 for file in $(find src -type f -name "*.dhall"); do
     filename=$(basename "$file" .dhall)
     filepath=$(dirname "$file" | sed 's|^src/||')
-    import=$(echo "$filepath" | tr '/' '.')
-
-    if [[ $counter -eq 1 ]]; then
-      prefix="{"
-    else 
-      prefix=","
-    fi
-
-    echo "$prefix $import.$filename = ./$file" >> "$output_file"
 
     mkdir -p releases/$latest_tag/docs/src/$filepath
     
@@ -45,17 +39,24 @@ for file in $(find src -type f -name "*.dhall"); do
     
     ((counter++))
 done
-echo "}" >> "$output_file"
+
+cp out/package.dhall "$output_file"
 
 ./release/dhall-to-html.sh "$output_file" "releases/$latest_tag/docs/package.dhall.html" "$package_name" "$latest_tag" $page_root
 
-CHECKSUM=$(sha256sum "$output_file" | awk '{print $1}')
+CHECKSUM=$(cat out/package.dhall.hash)
 
-echo ls --recursive releases
-
-echo "Generated $output_file with checksum $CHECKSUM"
+# Target examples to http import
+find examples -type f -name "Base.dhall" | while read -r file; do
+    echo "${page_root}/${latest_tag}/package.dhall ${CHECKSUM}" > "$file"
+    
+    if [[ $? -ne 0 ]]; then
+        echo "Error: update failed for $file"
+        exit 1
+    fi
+done
 
 echo "Release $latest_tag is ready to be published"
 
-REPLACEMENT="<li>\n<a href=\"./${latest_tag}/package.dhall\">${latest_tag}/package.dhall</a>\n<p>Import this version with:</p>\n<pre><code>https://minaprotocol.github.io/dhall-base/${latest_tag}/package.dhall sha256:${CHECKSUM}</code></pre>\n<p>\n<p><a href=\"./${latest_tag}/changelog.md\"> Changelog </a></p>\n<p><a href=\"./${latest_tag}/docs/package.dhall.html\"> Documentation </a></p>\n</li>\n<!-- Add more releases as needed -->"
+REPLACEMENT="<li>\n<a href=\"./${latest_tag}/package.dhall\">${latest_tag}/package.dhall</a>\n<p>Import this version with:</p>\n<pre><code>https://minaprotocol.github.io/dhall-base/${latest_tag}/package.dhall ${CHECKSUM}</code></pre>\n<p>\n<p><a href=\"./${latest_tag}/changelog.md\"> Changelog </a></p>\n<p><a href=\"./${latest_tag}/docs/package.dhall.html\"> Documentation </a></p>\n \n<p><a href=\"github.com/o1labs/dhall-buildkite/main/examples/${latest_tag}\"> Examples </a></p>\n</li>\n<!-- Add more releases as needed -->"
 sed -i "s|<!-- Add more releases as needed -->|${REPLACEMENT}|" releases/index.html
